8e689b589ec162dd0f8908e840f6b8249b8fed95
Beşir Karaoğlu <50145106+5karaoglu@users.noreply.github.com>
2021-08-04 18:31:05 +0300

    initial commit

---
+++ app/src/main/java/com/besirkaraoglu/rickandmorty/application/RamApplication.kt
@@ -0,0 +1,8 @@
+package com.besirkaraoglu.rickandmorty.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RamApplication : Application(){
+}
